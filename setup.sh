ea42a6d00cfc796a7ebeac28153c7cb253f6bb68
Jon Haddad <jon@jonhaddad.com>
2015-07-03 08:43:23 -0700

    ratings table

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [ ! -d "ml-10M100K" ]; then
+    wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
+    unzip ml-10m.zip
+fi
 
-wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
-unzip ml-10m.zip
+if [ ! -f "spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar" ]; then
+    wget https://s3.amazonaws.com/haddad.public/spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar
+fi
 
-wget https://s3.amazonaws.com/haddad.public/spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar
 pip install -r requirements.txt
