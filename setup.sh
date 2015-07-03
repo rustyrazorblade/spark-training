c95475f8ca7a08bf3585a62ab892b8bbb7754620
Jon Haddad <jon@jonhaddad.com>
2015-07-02 19:22:40 -0700

    setup sql, building out examples

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
 unzip ml-10m.zip
 
-wget https://haddad.public.s3.amazonaws.com/spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar
+wget https://s3.amazonaws.com/haddad.public/spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar
 pip install -r requirements.txt
