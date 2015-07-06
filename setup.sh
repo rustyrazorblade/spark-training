06c188064868956eca6b9160443106fe3701d308
Jon Haddad <jon@jonhaddad.com>
2015-07-06 12:57:35 -0700

    lots of new exercises

--- setup.sh
+++ setup.sh
@@ -2,7 +2,14 @@
 if [ ! -d "ml-10M100K" ]; then
     wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
     unzip ml-10m.zip
+    
+    (cd ml-10M100K;
+    head -n 10000 ratings.dat > ratings.subset.dat)
 fi
+
+# if you want to try something more advanced, you can pull down a big dataset:
+#
+
 
 if [ ! -f "spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar" ]; then
     wget https://s3.amazonaws.com/haddad.public/spark-cassandra-connector-assembly-1.4.0-M1-SNAPSHOT.jar
