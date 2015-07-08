c8ed54a355caba22ba20d2845c85d192b88a2b75
Jon Haddad <jon@jonhaddad.com>
2015-07-08 12:11:32 -0700

    pulling out the subset of records for our tests

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@ if [ ! -d "ml-10M100K" ]; then
     wget http://files.grouplens.org/datasets/movielens/ml-10m.zip
     unzip ml-10m.zip
 
-    (cd ml-10M100K;
-    head -n 10000 ratings.dat > ratings.subset.dat)
 fi
 
+if [ ! -f "ml-10M100K/ratings.subset.dat" ]; then
+    (cd ml-10M100K;
+    head -n 100000 ratings.dat > ratings.subset.dat)
+fi
 # if you want to try something more advanced, you can pull down a big dataset:
 #
 
