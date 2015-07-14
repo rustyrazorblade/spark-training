ebb6cd850c173e785858c38cb10b2f843687480b
Jon Haddad <jon@jonhaddad.com>
2015-07-14 03:43:33 -0700

    local context, not getting the right timestamp though

--- stream.py
+++ stream.py
@@ -21,6 +21,15 @@ def create_writer(sql, keyspace, mode="append"):
                  options(table=table, keyspace=keyspace).save(mode="append")
     return writer
 
+
+# Lazily instantiated global instance of SQLContext
+def getSqlContextInstance(sparkContext):
+    if ('sqlContextSingletonInstance' not in globals()):
+        globals()['sqlContextSingletonInstance'] = SQLContext(sparkContext)
+    return globals()['sqlContextSingletonInstance']
+
+
+
 writer = create_writer(sql, "training")
 
 lines = stream.socketTextStream("127.0.0.1", 6000)
@@ -31,25 +40,28 @@ def process_ratings(time, rdd):
     print "============== %s ============" % str(time)
     #
     ts = now()
-    row_rdd = rdd.map(lambda (movie_id, user_id, rating, timestamp):
+    print ts
+    local_sql = getSqlContextInstance(rdd.context)
+
+    row_rdd = rdd.map(lambda (user_id, movie_id, rating, timestamp):
                           Row(movie_id=int(movie_id), user_id=int(user_id),
                               rating=int(rating), ts=ts))
 
 
-    df = sql.createDataFrame(row_rdd)
+    df = local_sql.createDataFrame(row_rdd)
     df.registerTempTable("ratings")
 
     # I want to get the average rating, and count of the number of ratings for each movie and persist it to cassandra
     from pyspark.sql import functions as F
 
-    movie_to_ts = sql.sql("select distinct movie_id, ts from ratings")
+    movie_to_ts = local_sql.sql("select distinct movie_id, ts from ratings")
     movie_to_ts.registerTempTable("movie_ts")
 
     # going to join this against itself
-    agg = sql.sql("SELECT movie_id, avg(rating) as a, count(rating) as c from ratings group by movie_id")
+    agg = local_sql.sql("SELECT movie_id, avg(rating) as a, count(rating) as c from ratings group by movie_id")
     agg.registerTempTable("movie_aggregates")
 
-    matched = sql.sql("select a.movie_id, b.ts, a.a, a.c from movie_aggregates a join movie_ts b on a.movie_id = b.movie_id  ")
+    matched = local_sql.sql("select a.movie_id, b.ts, a.a, a.c from movie_aggregates a join movie_ts b on a.movie_id = b.movie_id  ")
 
     writer(matched, "movie_stream_ratings")
 
