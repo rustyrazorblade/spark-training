722f1c2a75a223820285d88abd8560a01196a6eb
Jon Haddad <jon@jonhaddad.com>
2015-08-04 20:00:13 -0700

    ratings generator seems to be working well

--- ratings_generator.py
+++ ratings_generator.py
@@ -11,6 +11,8 @@ s.bind(('127.0.0.1', 6000))
 print "Socket bound"
 print "Opening movie ratings"
 
+start = time.time()
+total = 0
 with open("ml-10M100K/ratings.dat", 'r') as fp:
 
     print "Waiting for socket 6000"
@@ -25,6 +27,11 @@ with open("ml-10M100K/ratings.dat", 'r') as fp:
     # comma delimited
     print "Sending ratings"
     for line in fp:
+        total += 1
         (user_id, movie_id, rating, ts) = line.split("::")
         msg = "{}::{}::{}::{}\n".format(user_id, movie_id, rating, time.time())
         conn.sendall(msg)
+
+total_time = time.time() - start
+print "total time: {}".format(total_time)
+print "{}/s".format(float(total) / total_time)
